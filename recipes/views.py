fc51159e7aeb59dee4cb067c8f7d44ab5eb0f97d
Marcelo Teixeira Paiva <marcelo_thelin@hotmail.com>
2023-07-15 15:00:38 -0300

    recipe-view and heritage from base.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
