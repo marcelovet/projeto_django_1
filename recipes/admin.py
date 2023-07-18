015601abdbdcfcdba36915d96dcfabd03294a814
Marcelo Teixeira Paiva <marcelo_thelin@hotmail.com>
2023-07-18 11:50:28 -0300

    category and recipe models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from recipes import models
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(models.Category, CategoryAdmin)
